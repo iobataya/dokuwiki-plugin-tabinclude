b2bed1a11c9f1bc514175108c29bfe002d87e4c1
Ikuo Obataya <i.obataya@gmail.com>
2013-05-03 13:39:58 +0900

    Brazilian Portuguese translation added
    Language files of Brazilian Portuguese(pt-br) from Benedito Carneiro
    were added.

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,3 @@
+<?php
+$lang['loading']     = 'Carregando...';
+$lang['gotohere']    = '>> Editar esta página.';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['hideloading']      = 'Não exibir o aviso de carregamento da página';
+$lang['goto_link_header'] = 'Exibir o link para a página no cabeçalho';
+$lang['goto_link_footer'] = 'Exibir o link para a página no rodapé';
