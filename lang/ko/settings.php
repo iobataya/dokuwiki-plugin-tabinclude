a26b2cefc5a7434da98804709eca6398c50e0d9b
Myeongjin <aranet100@gmail.com>
2013-08-15 05:30:39 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['use_first_heading']     = '탭에서 1단계 문단 제목 사용';
+$lang['namespace_in_tab']      = '문서 ID에 대한 이름공간 포함';
+$lang['hideloading']           = '불러오는 중 메시지 숨기기';
+$lang['goto_link_header']      = '머리글에 문서 링크 보이기';
+$lang['goto_link_footer']      = '바닥글에 문서 링크 보이기';
