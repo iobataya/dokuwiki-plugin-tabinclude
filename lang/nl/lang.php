9c8d4f3bfc90a71fed5534b5761ce994f2e9953b
Mark Prins <mprins@users.sf.net>
2013-08-22 10:40:05 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark Prins <mprins@users.sf.net>
+ */
+$lang['loading']               = 'Laden...';
+$lang['gotohere']              = '>> Ga naar deze pagina.';
+$lang['error_notfound']        = 'Pagina niet gevonden.';
+$lang['error_parent']          = 'De ouderpagina kan niet worden getoond!';
