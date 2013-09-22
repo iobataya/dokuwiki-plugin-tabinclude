78a5f45d40438cd496819345c6cfc32aa0ce9963
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-09-22 02:00:11 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['loading']               = 'Загрузка...';
+$lang['gotohere']              = '>> Перейти на страницу';
+$lang['error_notfound']        = 'Страница не найдена.';
---
+++ lang/ru/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['hideloading']           = 'Скрыть сообщение о загрузке';
