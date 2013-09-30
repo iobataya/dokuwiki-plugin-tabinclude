117235eb964ef27a8aa58239c200c1b49393bbcd
Matthias Schulte <dokuwiki@lupo49.de>
2013-09-30 13:40:27 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Matthias Schulte <dokuwiki@lupo49.de>
+ */
+$lang['loading']               = 'Laden...';
+$lang['gotohere']              = '>> Diese Seite aufrufen.';
+$lang['error_notfound']        = 'Seite nicht gefunden.';
+$lang['error_parent']          = 'Übergeordnete Seite kann nicht angezeigt werden!';
