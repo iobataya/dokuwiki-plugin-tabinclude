79df5af59244e40818592d4bef9e6e6283755b87
Robert Bogenschneider <bogi@uea.org>
2014-03-01 08:45:05 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['use_first_heading']     = 'Uzi la unuan temlinion en tabelo';
+$lang['namespace_in_tab']      = 'Inkludi nomspacojn por paĝ-ID';
+$lang['hideloading']           = 'Kaŝi la ŝarĝ-mesaĝon';
+$lang['goto_link_header']      = 'Montri paĝligilon kape';
+$lang['goto_link_footer']      = 'Montri paĝligilon piede';
