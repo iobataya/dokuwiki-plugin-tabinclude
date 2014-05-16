1097f3857bbf0aa05f6f4c0b06e2729dc44d8a73
İlker Rifat Kapaç <irifat@gmail.com>
2014-05-16 09:10:05 +0200

    translation update

---
+++ lang/tr/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker Rifat Kapaç <irifat@gmail.com>
+ */
+$lang['loading']               = 'Yükleniyor...';
+$lang['gotohere']              = '>> Bu sayfaya git.';
+$lang['error_notfound']        = 'Sayfa bulunamadı.';
+$lang['error_parent']          = 'Ebeveyn sayfa gösterilemedi!';
