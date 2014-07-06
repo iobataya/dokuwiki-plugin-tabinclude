2ea1037fe7f16aa0c9e3f217acfb868624e0f909
Robert Bogenschneider <bogi@uea.org>
2014-07-06 20:15:22 +0200

    translation update

--- lang/eo/settings.php
+++ lang/eo/settings.php
@@ -8,5 +8,6 @@
 $lang['use_first_heading']     = 'Uzi la unuan temlinion en tabelo';
 $lang['namespace_in_tab']      = 'Inkludi nomspacojn por paĝ-ID';
 $lang['hideloading']           = 'Kaŝi la ŝarĝ-mesaĝon';
+$lang['ajax_init_page']        = 'Legi la komencan langeto-paĝon tra AJAX (AJAX-moduso)';
 $lang['goto_link_header']      = 'Montri paĝligilon kape';
 $lang['goto_link_footer']      = 'Montri paĝligilon piede';
