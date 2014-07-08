40c3fe0168a5505dcbc92d71dad48d1f0a1f7e39
mprins <mprins@users.sf.net>
2014-07-08 20:05:14 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Mark Prins <mprins@users.sf.net>
+ * @author mprins <mprins@users.sf.net>
  */
 $lang['use_first_heading']     = 'Gebruik eerste kop in tabblad';
 $lang['namespace_in_tab']      = 'Gebruik naamruimte voor pagina ID';
 $lang['hideloading']           = 'Verberg het laden bericht';
+$lang['ajax_init_page']        = 'Laadt het initiële tabblad mbv. AJAX (AJAX modus) ';
 $lang['goto_link_header']      = 'Toon pagina link in kopregel';
 $lang['goto_link_footer']      = 'Toon pagina link in voetnoot';
