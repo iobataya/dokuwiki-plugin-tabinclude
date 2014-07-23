4e62315fdb86ba802fbb78e40bf1c2dad5a29b96
tuxun <tuxuntrash@gmail.com>
2014-07-23 02:10:09 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Fabrice Dejaigher <fabrice@chtiland.com>
+ * @author tuxun <tuxuntrash@gmail.com>
  */
 $lang['use_first_heading']     = 'Utiliser le premier titre dans l\'onglet';
-$lang['namespace_in_tab']      = 'Inclure les namespaces pour les ID de page';
-$lang['hideloading']           = 'Masquer le message de chargement';
+$lang['namespace_in_tab']      = 'Inclure les espaces de noms dans les ID de page';
+$lang['hideloading']           = 'Masquer le message de chargement (mode  AJAX)';
+$lang['ajax_init_page']        = 'Page à charger initialement par AJAX (mode AJAX)';
 $lang['goto_link_header']      = 'Afficher le lien vers la page dans l\'entête';
-$lang['goto_link_footer']      = 'Afficher le lien vers la page dans le pied';
+$lang['goto_link_footer']      = 'Afficher le lien vers la page dans le pied de page';
