e0b6b46a3452750cf434dc690d65ffd2b6dd068b
Ikuo Obataya <i.obataya@gmail.com>
2014-08-17 23:12:08 +0900

    Update settings.php
    added show TOC tab

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,3 +5,4 @@ $lang['hideloading']      = 'Hide loading message (AJAX mode)';
 $lang['ajax_init_page']   = 'Read initial tab page by AJAX (AJAX mode)';
 $lang['goto_link_header'] = 'Show page link at header';
 $lang['goto_link_footer'] = 'Show page link at footer';
+$lang['toc_in_tab']       = 'Show TOC in tab pages';
