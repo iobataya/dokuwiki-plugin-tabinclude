c668b262e9ecc3cb58f4c07a8fc5704fd18358f7
Hideaki SAWADA <chuno@live.jp>
2014-09-01 21:05:24 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -1,7 +1,14 @@
 <?php
-$lang['use_first_heading']= 'タブには最初の見出しを使う';
-$lang['namespace_in_tab'] = 'ページ名には名前空間も含める';
-$lang['ajax_init_page']   = '初期ページにAJAXを使う (AJAXモード)';
-$lang['hideloading']      = 'Loadingメッセージを隠す (AJAXモード)';
-$lang['goto_link_header'] = 'リンクを表示(ヘッダ)';
-$lang['goto_link_footer'] = 'リンクを表示(フッタ)';
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['use_first_heading']     = 'タブには最初の見出しを使う';
+$lang['namespace_in_tab']      = 'ページ名には名前空間も含める';
+$lang['hideloading']           = 'Loadingメッセージを隠す (AJAXモード)';
+$lang['ajax_init_page']        = '初期ページにAJAXを使う (AJAXモード)';
+$lang['goto_link_header']      = 'リンクを表示(ヘッダ)';
+$lang['goto_link_footer']      = 'リンクを表示(フッタ)';
+$lang['toc_in_tab']            = 'タブページに目次(TOC)を表示する';
