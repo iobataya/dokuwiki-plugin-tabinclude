ee0d088f900a08d3203311aa5c6e6acf4ce321e0
Søren Birk <soer9648@eucl.dk>
2014-12-15 14:45:37 +0100

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['use_first_heading']     = 'Benyt titel i fane';
+$lang['namespace_in_tab']      = 'Inkludér navnerum for side-ID';
+$lang['hideloading']           = 'Skjul indlæser-meddelelse (AJAX-metode)';
+$lang['ajax_init_page']        = 'Læse første faneside af AJAX (AJAX-metode)';
+$lang['goto_link_header']      = 'Vis sidelink ved sidehoved';
+$lang['goto_link_footer']      = 'Vis sidelink ved sidefod';
+$lang['toc_in_tab']            = 'Vis indholdsfortegnelse i fanesider';
