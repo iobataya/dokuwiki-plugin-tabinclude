7081f5b6a6c1ab3fa2f91590f9f3875015f9fc3e
Wouter Wijsman <wwijsman@live.nl>
2015-01-05 14:55:48 +0100

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -5,6 +5,7 @@
  * 
  * @author Mark Prins <mprins@users.sf.net>
  * @author mprins <mprins@users.sf.net>
+ * @author Wouter Wijsman <wwijsman@live.nl>
  */
 $lang['use_first_heading']     = 'Gebruik eerste kop in tabblad';
 $lang['namespace_in_tab']      = 'Gebruik naamruimte voor pagina ID';
@@ -12,3 +13,4 @@ $lang['hideloading']           = 'Verberg het laden bericht';
 $lang['ajax_init_page']        = 'Laadt het initiële tabblad mbv. AJAX (AJAX modus) ';
 $lang['goto_link_header']      = 'Toon pagina link in kopregel';
 $lang['goto_link_footer']      = 'Toon pagina link in voetnoot';
+$lang['toc_in_tab']            = 'Toon inhoudsopgave in tabbladen';
