c8d5360c95df7f95447a6fe74b3af9529859eb7e
Hanjia Zhuang <shzhuanghanjia@163.com>
2015-01-26 02:50:14 +0100

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hanjia Zhuang <shzhuanghanjia@163.com>
+ */
+$lang['loading']               = '加载中...';
+$lang['gotohere']              = '>>前往此页面';
+$lang['error_notfound']        = '页面未找到';
