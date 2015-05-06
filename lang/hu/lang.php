8138eef4e3d2fbdb0a9be68fdbc29504f02734b7
DelD <deldadam@gmail.com>
2015-05-06 22:55:16 +0200

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author DelD <deldadam@gmail.com>
+ */
+$lang['loading']               = 'Töltés...';
+$lang['gotohere']              = '>> Ugrás az oldalra.';
+$lang['error_notfound']        = 'Az oldal nem található.';
+$lang['error_parent']          = 'A szülő oldal nem jeleníthető meg.';
