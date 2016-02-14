736e015a358d8644933a8ac88ad8f2606570fb1e
Ikuo Obataya <i.obataya@gmail.com>
2016-02-14 22:20:22 +0900

    Update settings.php
    liz's translation was applied.

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -7,11 +7,12 @@
  * @author Egmont Schreiter <egmont.schreiter@gmx.de>
  * @author Padhie <develop@padhie.de>
  * @author Dana <dannax3@gmx.de>
+ * @author liz <marliza@web.de>
  */
 $lang['use_first_heading']     = 'Erste Überschrift im Tab verwenden';
 $lang['namespace_in_tab']      = 'Namespaces für Seite mit folgender ID einschließen';
-$lang['hideloading']           = 'Fortschrittsanzeige nicht anzeigen';
-$lang['ajax_init_page']        = 'Lesen der initalen Tab-Seite von AJAX (AJAX Modus)';
+$lang['hideloading']           = 'Fortschrittsanzeige (AJAX mode) ausblenden';
+$lang['ajax_init_page']        = 'Ursprüngliche tab-Seite von AJAX (AJAX mode) lesen';
 $lang['goto_link_header']      = 'Seitenadresse in der Kopfzeile anzeigen';
 $lang['goto_link_footer']      = 'Seitenadresse in der Fußzeile anzeigen';
-$lang['toc_in_tab']            = 'Zeige TOC im Tab-Seite';
+$lang['toc_in_tab']            = 'TOC in Tab-Seiten anzeigen';
