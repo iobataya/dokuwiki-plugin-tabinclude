ef20c1f1d8d81237adac4818770484e36c968f5c
Myeongjin <aranet100@gmail.com>
2016-07-27 12:57:12 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -2,10 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['loading']               = '불러오는 중 ...';
 $lang['gotohere']              = '>> 이 문서로 갑니다.';
 $lang['error_notfound']        = '문서를 찾을 수 없습니다.';
-$lang['error_parent']          = '상위 문서는 보여줄 수 없습니다 !';
+$lang['error_parent']          = '상위 문서를 표시할 수 없습니다 !';
