4c99363cd2db52c298a5351dc0a78464fb64e0f9
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-19 22:25:33 +0200

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ *
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-$lang['use_first_heading']     = 'Использовать первый заголовок в табах';
+$lang['use_first_heading']     = 'Использовать первый заголовок на&nbsp;вкладках';
 $lang['hideloading']           = 'Скрыть сообщение о загрузке (AJAX-режим)';
+$lang['goto_link_header']      = 'Показывать ссылку сверху включаемой страницы';
+$lang['goto_link_footer']      = 'Показывать ссылку внизу включаемой страницы';
+$lang['toc_in_tab']            = 'Показывать содержание влючаемых страниц';
