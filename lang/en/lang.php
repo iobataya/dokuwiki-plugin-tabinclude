100668b35323707979efbda60e9784d11b7402b8
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-19 21:55:17 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,5 +1,11 @@
 <?php
-$lang['loading']     = 'Loading ...';
-$lang['gotohere']    = '>> Go to this page.';
-$lang['error_notfound'] = 'Page not found.';
-$lang['error_parent'] = 'Parent page cannot be displayed !';
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['loading']               = 'Loading...';
+$lang['gotohere']              = '>> Go to this page.';
+$lang['error_notfound']        = 'Page not found.';
+$lang['error_parent']          = 'Parent page cannot be displayed!';
